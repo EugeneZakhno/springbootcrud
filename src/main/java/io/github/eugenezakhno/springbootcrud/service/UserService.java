3df9f793ff7dfcf2e55e5677c339103998b85517
EugeneZakhno <zohan1234567@gmail.com>
2020-01-07 23:21:06 +0300

    added UserService

---
+++ src/main/java/io/github/eugenezakhno/springbootcrud/service/UserService.java
@@ -0,0 +1,15 @@
+package io.github.eugenezakhno.springbootcrud.service;
+
+import io.github.eugenezakhno.springbootcrud.model.User;
+
+public class UserService {
+
+    public User findById(Long id){
+
+    }
+    public List<User> findAll(){
+
+    }
+    public User saveUser()
+
+}
