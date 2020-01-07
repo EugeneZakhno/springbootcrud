dabdd5ee177c8002ead5332543e74ed0bf66b4dd
EugeneZakhno <zohan1234567@gmail.com>
2020-01-07 23:24:44 +0300

    added methods into UserService

--- src/main/java/io/github/eugenezakhno/springbootcrud/service/UserService.java
+++ src/main/java/io/github/eugenezakhno/springbootcrud/service/UserService.java
@@ -1,15 +1,25 @@
 package io.github.eugenezakhno.springbootcrud.service;
 
 import io.github.eugenezakhno.springbootcrud.model.User;
+import io.github.eugenezakhno.springbootcrud.repository.UserRepository;
+
+import java.util.List;
 
 public class UserService {
 
-    public User findById(Long id){
+    private UserRepository userRepository;
 
+    public User findById(Long id){
+        return userRepository.getOne(id);
     }
     public List<User> findAll(){
 
     }
-    public User saveUser()
+    public User saveUser(User user){
+
+    }
+    public void deleteById(Long id){
+
+    }
 
 }
