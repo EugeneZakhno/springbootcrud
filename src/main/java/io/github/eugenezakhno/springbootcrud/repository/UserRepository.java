94ba4ba52241a88700430c8f21dfa5d7e664ae54
EugeneZakhno <zohan1234567@gmail.com>
2020-01-07 22:43:57 +0300

    added UserRepository

---
+++ src/main/java/io/github/eugenezakhno/springbootcrud/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package io.github.eugenezakhno.springbootcrud.repository;
+
+import io.github.eugenezakhno.springbootcrud.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findByLastName (String lastName);
+
+
+
+}
