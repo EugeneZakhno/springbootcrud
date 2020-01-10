239b88311569960b690b97316aa58025194afb07
EugeneZakhno <zohan1234567@gmail.com>
2020-01-10 23:32:54 +0300

    refactoring User

--- src/main/java/io/github/eugenezakhno/springbootcrud/model/User.java
+++ src/main/java/io/github/eugenezakhno/springbootcrud/model/User.java
@@ -19,7 +19,7 @@ public class User {
     private Long id;
 
     @Column(name = "first_name")
-    private String firstName = "vasia";
+    private String firstName;
     @Column(name = "last_name")
     private String lastName;
 
