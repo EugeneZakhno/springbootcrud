005af01f0ce752b6eaa84f23b4cfb3896a83710f
EugeneZakhno <zohan1234567@gmail.com>
2020-01-18 00:39:52 +0300

    refactoring 05 style.css

--- src/main/java/io/github/eugenezakhno/springbootcrud/model/User.java
+++ src/main/java/io/github/eugenezakhno/springbootcrud/model/User.java
@@ -17,8 +17,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(name = "first_name")
+    private int number;
+    @Column(name = "number")
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
