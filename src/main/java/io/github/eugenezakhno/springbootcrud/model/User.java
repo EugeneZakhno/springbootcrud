9f7d5ab04d662ab47b046dd42d94ebc1f34e6219
EugeneZakhno <zohan1234567@gmail.com>
2020-01-19 00:20:50 +0300

    added new column number

--- src/main/java/io/github/eugenezakhno/springbootcrud/model/User.java
+++ src/main/java/io/github/eugenezakhno/springbootcrud/model/User.java
@@ -17,10 +17,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "first_name")
-    private int number;
+
     @Column(name = "number")
+    private int number;
+
+    @Column(name = "first_name")
     private String firstName;
+
     @Column(name = "last_name")
     private String lastName;
 
